135d9b9ffd2f6aef58f0ba1855bc2d13b66efd09
toor <jeck.abolfia@gmail.com>
2020-12-29 03:12:32 -0500

    Made progress towards parsing each session's data.

--- routes/config.js
+++ routes/config.js
@@ -1,18 +1,52 @@
 const iniParser = require('../utils/ini-reader')
+const fs = require('fs')
+const path = require('path')
 
+
+let configFiles = {}
+
+function traverseDir(dir) {
+    fs.readdirSync(dir).forEach(file => {
+      let fullPath = path.join(dir, file);
+      let parentDir = path.basename(fullPath)
+      if (fs.lstatSync(fullPath).isDirectory()) {
+          parent
+          configFiles[parentDir] = []
+          console.log(file)
+         traverseDir(fullPath);
+       } else {
+           configFiles[parentDir]
+       }  
+    });
+  }
 
 module.exports = function (app) {
     app.get('/sessions', (req, res) => {
 
+        const sessionsRootPath = 'Sessions/'
+        traverseDir(sessionsRootPath)
+
+        for(const [key, value] of Object.entries(configFiles)) {
+            console.log(value)
+        }
+
+    
+        let config = iniParser('Sessions/Session1/publicTX.conf')
+
         const session = JSON.stringify({
             "sessions": [
-                {
-                    "name": "TX1",
-                    "bandwidth": "5.0"
-                }
+                config
             ]
         })
 
+        // const session = JSON.stringify({
+        //     "sessions": [
+        //         {
+        //             "name": "TX1",
+        //             "bandwidth": "5.0"
+        //         }
+        //     ]
+        // })
         res.end(session)
     })
 }
--- utils/ini-reader.js
+++ utils/ini-reader.js
@@ -1,6 +1,6 @@
 const ini = require('ini')
 const fs = require('fs')
 
-let config = ini.parse(fs.readFileSync('Sessions/Session1/publicTX.conf', 'utf-8'))
-
-console.log(config)
+module.exports = (configPath) => {
+    return ini.parse(fs.readFileSync(configPath, 'utf-8'))
+}
